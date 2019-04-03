bdc103f19ee91448fb64a9f59414852753cc7fc9
julienmalard <julien.malard@mail.mcgill.ca>
2019-04-03 17:35:27 -0400

    Premiers fichiers

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='swatpyplus',
+    version='0',
+    packages=[''],
+    url='swatpyplus.rtfd.io',
+    license='GNU Affero GPL v3',
+    author='julienmalard',
+    author_email='julien.malard@mail.mcgill.ca',
+    description='Communication avec SWAT+, en Python'
+)
