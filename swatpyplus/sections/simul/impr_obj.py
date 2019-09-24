2799c31f40205dc609a97a2d2363f53a01fabeca
James Julius <james.julius@mail.mcgill.ca>
2019-09-24 14:02:43 -0400

    added a few new files

--- swatpyplus/sections/simul/impr_obj.py
+++ swatpyplus/sections/simul/impr_obj.py
@@ -1,5 +1,37 @@
 from swatpyplus.fichier import Fichier
+from swatpyplus.variables import VarEntier, VarTexte
+
+
+class Numéro(VarEntier):
+    nom = 'Numéro'
+    cod = 'numb'
+
+
+class TypeObjet(VarTexte):
+    nom = "Type d'objet à imprimer"
+    cod = 'obtyp'
+
+
+class NumTypeObjet(VarEntier):
+    nom = "Numéro de type d'objet"
+    cod = 'obtypno'
+
+
+class Hydrogramme(VarTexte):
+    nom = "Type d'hydrogramme à imprimer"
+    cod = 'hydtyp'
+
+
+class NomFichier(VarTexte):
+    nom = "Nom du fichier du sortie"
+    cod = 'filename'
+
+
+vars_ = [
+    Numéro, TypeObjet, NumTypeObjet, Hydrogramme, NomFichier
+]
 
 
 class ImprimerObjet(Fichier):
     nom = 'object.prt'
+    variables = vars_
--- swatpyplus/sections/sols/sect.py
+++ swatpyplus/sections/sols/sect.py
@@ -1,7 +1,10 @@
 from swatpyplus.sect import Section
+from .comp import Composantes
+from .compte_obj import CompteObjet
 from .charac import CharacSols
+from .nutriments import NutriSol
 
 
 class Sols(Section):
     nom = 'soils'
-    fichiers = [CharacSols, ]
+    fichiers = [CharacSols, NutriSol, CompteObjet, Composantes]
